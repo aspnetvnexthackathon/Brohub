4ffabc60a7b0079e8247e432c49a2ebe567436da
Javier Calvarro Nelson <jacalvar@microsoft.com>
2014-06-25 09:23:11 -0700

    Skeleton solution

---
+++ Brohub/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+
+namespace Brohub
+{
+    /// <summary>
+    /// Summary description for HomeController
+    /// </summary>
+    public class HomeController : Controller
+    {
+	    public HomeController()
+	    {
+	    }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Brohub/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Routing;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Brohub
+{
+    public class Startup
+    {
+        public void Configure(IBuilder app)
+        {
+            app.UseServices(services =>
+            {
+                services.AddMvc();
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller}/{action}",new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
