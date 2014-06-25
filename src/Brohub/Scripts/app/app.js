68ef30332934bfcce4948b60fa735ff994191eea
Javier Calvarro Nelson <jacalvar@microsoft.com>
2014-06-25 10:59:59 -0700

    Added angular routing

---
+++ src/Brohub/Scripts/app/app.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="../angular/angular.js" />
+/// <reference path="../angular/angular-route.js" />
+/// <reference path="../jquery-2.1.1.js" />
+var Brohub = (function (angular, $) {
+    var brohub = angular.module('Brohub', ['ngRoute']);
+
+    brohub.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+        .when('/',
+        {
+            templateUrl: 'Views/Home/Index.html',
+            controller: 'HomeController',
+            controllerAs: 'homeCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+    }]);
+
+    brohub.controller('HomeController', function () {
+        this.bro = 'Bro from Home';
+    });
+
+})(angular, jQuery);
