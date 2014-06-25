0583f3cb7731133eadef2a3d129b00d7b43354fc
NTaylorMullen <nimullen@microsoft.com>
2014-06-25 12:34:39 -0700

    Added automagic git cloning from command line.

--- src/Brohub.Console/Program.cs
+++ src/Brohub.Console/Program.cs
@@ -1,13 +1,47 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using LibGit2Sharp;
 
-namespace Brohub.Console
+namespace Brohub
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World");
-            System.Console.ReadLine();
+            if (args.Length == 0 || !args[0].StartsWith("https://github.com/"))
+            {
+                Console.WriteLine("A GitHub clone url is required. Ex: https://github.com/aspnet/Mvc.git");
+            }
+
+            string gitCloneUrl = args[0];
+            string gitClonePath = "./" + gitCloneUrl.Split('/').Last().Replace(".git", string.Empty);
+
+            DeleteDirectory(gitClonePath);
+
+            Repository.Clone(gitCloneUrl, "./" + gitClonePath);
+
+            using(var localRepo = new Repository(gitClonePath))
+            {
+                // Do what you want with git stuff.
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void DeleteDirectory(string directory)
+        {
+            if (!Directory.Exists(directory))
+            {
+                return;
+            }
+
+            var shell = PowerShell.Create();
+
+            shell.AddScript("Remove-Item -Force -Recurse \"" + directory + "\"");
+
+            shell.Invoke();
         }
     }
 }
